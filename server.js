5f00f2e6a00e5df4ad47e689cf876a2492c7d833
soumakArgha <soumak.argha@gmail.com>
2018-09-19 01:18:34 +0530

    server updated

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express()
 
 const port = process.env.PORT || 5000
 
-app.use(express.static(path.join(__dirname, "build")))
+app.use(express.static(path.join(__dirname)))
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build/index.html'));
+    res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {

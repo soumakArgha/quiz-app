0fb9c189814bc5afd68ccdf66ffc15a667418be0
soumakArgha <soumak.argha@gmail.com>
2018-09-19 01:16:11 +0530

    server updated

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const port = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, "build")))
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'build/index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build/index.html'));
+});
 
 app.listen(port, () => {
     console.log("Server started at " + port)

a6e18fb4665273f22fa017257fca79e7daffd767
soumakArgha <soumak.argha@gmail.com>
2018-09-19 01:13:01 +0530

    port updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path')
 const app = express()
 
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, "build")))
 
